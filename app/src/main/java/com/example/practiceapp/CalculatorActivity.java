62d8a33e39ce833573952e6aa350234630494bbd
Parth Shah <parth3146@gmail.com>
2021-06-08 23:45:18 +0530

    Calculator

--- app/src/main/java/com/example/practiceapp/CalculatorActivity.java
+++ app/src/main/java/com/example/practiceapp/CalculatorActivity.java
@@ -7,35 +7,70 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class CalculatorActivity extends AppCompatActivity {
+public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText et1,et2;
-    Button btnSubmit;
+    Button btnAdd,btnSub,btnMul,btnDiv;
     TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
-        et1 = findViewById(R.id.et1);
-        et2 = findViewById(R.id.et2);
-        btnSubmit = findViewById(R.id.subbt);
-        tv = findViewById(R.id.tv);
 
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String n1 = et1.getText().toString();
-                String n2 = et2.getText().toString();
-                int num1 = Integer.parseInt(n1);
-                int num2 = Integer.parseInt(n2);
-                int sum =  num1 + num2;
-                tv.setText(sum);
-                //Toast.makeText(getApplicationContext(), sum, Toast.LENGTH_SHORT).show();
-            }
-        });
+        et1=findViewById(R.id.etNum1);
+        et2=findViewById(R.id.etNum2);
+
+        btnAdd=findViewById(R.id.btnAdd);
+        btnDiv=findViewById(R.id.btnDiv);
+        btnSub=findViewById(R.id.btnSub);
+        btnMul=findViewById(R.id.btnMul);
+
+        tv=findViewById(R.id.tvAns);
+
+        btnMul.setOnClickListener(this);
+        btnSub.setOnClickListener(this);
+        btnDiv.setOnClickListener(this);
+        btnAdd.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        String num1,num2;
+
+        num1 = et1.getText().toString();
+        num2 = et2.getText().toString();
+
+        int n1,n2,ans;
+
+        n1 = Integer.parseInt(num1);
+        n2 = Integer.parseInt(num2);
+
+        switch (v.getId())
+        {
+            case R.id.btnAdd:
+                ans = n1+n2;
+                break;
+
+            case R.id.btnSub:
+                ans = n1-n2;
+                break;
+
+            case R.id.btnMul:
+                ans = n1*n2;
+                break;
+
+            case R.id.btnDiv:
+                ans = n1/n2;
+                break;
+
+            default:
+                ans = 0;
+        }
+
+        tv.setText("Answer is "+ans);
 
     }
 }
--- app/src/main/java/com/example/practiceapp/MainActivity.java
+++ app/src/main/java/com/example/practiceapp/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.practiceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button calcBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        calcBtn = findViewById(R.id.calcBtn);
+        calcBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CalculatorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
