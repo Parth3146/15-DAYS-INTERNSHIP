a522786f68cf044c0c590d9dc3d62dcdeadc3f21
Parth Shah <parth3146@gmail.com>
2021-06-09 12:50:37 +0530

    GridView

--- app/src/main/java/com/example/practiceapp/MainActivity.java
+++ app/src/main/java/com/example/practiceapp/MainActivity.java
@@ -5,22 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button calcBtn;
+    GridView gridView;
+    String activities[] = {"Calculator","WebView","MusicPlayer","VideoPlayer"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        calcBtn = findViewById(R.id.calcBtn);
-        calcBtn.setOnClickListener(new View.OnClickListener() {
+
+         gridView = findViewById(R.id.gv);
+        ArrayAdapter arrayAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,activities);
+        gridView.setAdapter(arrayAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CalculatorActivity.class);
-                startActivity(intent);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent;
+                switch (position){
+                    case 0:
+                        intent = new Intent(MainActivity.this, CalculatorActivity.class);
+                        startActivity(intent);
+                        break;
+                    default:
+                        Toast.makeText(MainActivity.this, "You selected "+activities[position], Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
     }
