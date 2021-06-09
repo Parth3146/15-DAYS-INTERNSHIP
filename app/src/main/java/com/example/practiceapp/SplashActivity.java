4afdf4c8d0a7379fd9aa34dff6316eb2e4811fe8
Parth Shah <parth3146@gmail.com>
2021-06-09 12:03:19 +0530

    SplashScreen Updated

--- app/src/main/java/com/example/practiceapp/SplashActivity.java
+++ app/src/main/java/com/example/practiceapp/SplashActivity.java
@@ -3,18 +3,34 @@ package com.example.practiceapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_splash);
+
+
+        mediaPlayer = MediaPlayer.create(SplashActivity.this, R.raw.scambgm);
+        mediaPlayer.start();
+
         Thread thread = new Thread() {
           public void run() {
               try {
-                  sleep(3000);
+                  sleep(10000);
               } catch (Exception e) {
                   e.printStackTrace();
               } finally {
@@ -29,6 +45,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        mediaPlayer.release();
         finish();
     }
 }
